333a6b4ab44f3a84a6efacb6ad2d140d40276b0a
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-07-17 15:49:30 +0200

    Make stat card clickable

--- public/main.js
+++ public/main.js
@@ -57,7 +57,7 @@ function createStatCard(item, timeframeLabel) {
   statCard.innerHTML = `
     <div class="stat-card__container">
       <div class="stat-card__header">
-        <h3 class="stat-card__title">${item.title}</h3>
+        <a href="#" class="stat-card__title">${item.title}</a>
         <button class="stat-card__menu-toggle" aria-label="menu - ${item.title}" aria-expanded="false">···</button>
       </div>
       <div class="stat-card__timeframes">
